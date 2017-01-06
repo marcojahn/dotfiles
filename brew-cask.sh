87aa98edcc2640a4eff00a337f5a0e1db5189246
Marco Jahn <marco.jahn@gmail.com>
2017-01-06 11:47:24 +0100

    moved cask installations to .sh file

---
+++ brew-cask.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+brew cask install adium
+brew cask install cocoarestclient
+brew cask install cyberduck
+brew cask install firefox
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install macdown
+brew cask install smcfancontrol
+brew cask install sublime-text
+brew cask install vlc
+
+brew tap caskroom/versions
+
+brew cask install firefoxdeveloperedition
